8d03fea1b900104fbc695b378ca94430782c61c4
ireneminnozzi <irene.minnozzi@studenti.unicam.it>
2026-07-03 17:22:33 +0200

    UPDATE: creazione classi Boss

---
+++ src/main/java/it/unicam/cs/mpgc/rpg130957/model/combat/BossType.java
@@ -0,0 +1,17 @@
+package it.unicam.cs.mpgc.rpg130957.model.combat;
+
+public enum BossType {
+    DRAGO_PRIMORDIALE(400, 45),
+    SPIRITO_ANTICO(250, 30);
+
+    private final int saluteBase;
+    private final int dannoBase;
+
+    BossType(int saluteBase, int dannoBase) {
+        this.saluteBase = saluteBase;
+        this.dannoBase = dannoBase;
+    }
+
+    public int getSaluteBase() { return saluteBase; }
+    public int getDannoBase() { return dannoBase; }
+}
